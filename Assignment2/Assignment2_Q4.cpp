1096032756a05fb7e6706eb4fe94f863244df307
Dhruv Mehta <61137052+Dhruv-194@users.noreply.github.com>
2020-08-27 00:55:04 +0530

    Assignment2
    Assignment-2 q4

---
+++ Assignment2/Assignment2_Q4.cpp
@@ -0,0 +1,46 @@
+//Find the sum of all the natural numbers up to n
+
+
+// C++ program to find sum of natural numbers without using recursion
+#include <iostream>
+using namespace std;
+int sum_of_natural_numbers(int n)
+{
+int sum = 0;
+for(int i = 1; i <= n; i++)
+{
+sum += i;
+}
+return sum;
+}
+int main()
+{
+int n;
+cout << “\nEnter the number : “;
+cin >> n;
+cout << “\nSum of ” << n << ” Natural Numbers is ” << sum_of_natural_numbers(n);
+cout << endl;
+return 0;
+}
+
+// C++ program to find sum of natural numbers using recursion
+
+#include <iostream>
+using namespace std;
+int sum_of_natural_numbers(int n)
+{
+if(n == 0)
+return 0;
+return n + sum_of_natural_numbers(n – 1);
+}
+
+int main()
+{
+int n;
+cout << “\nEnter the number : “;
+cin >> n;
+cout << “\nSum of ” << n << ” Natural Numbers is ” << sum_of_natural_numbers(n);
+cout << endl;
+return 0;
+}
+
